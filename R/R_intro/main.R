778556fead8afe0620e9a69c3dded608f5227c45
wenduowang <wenduo.wang@hotmail.com>
2016-08-08 23:16:57 -0500

    Reorganized space

--- R/R_intro/main.R
+++
@@ -1,9 +0,0 @@
-x=sort(rnorm(100))
-y=x
-f = outer(x, y, function(x,y)cos(y)/(1+x^2))
-contour(x,y,f)
-contour(x,y,f,nlevels=45,add=T)
-fa = (f-t(f))/2
-contour(x,y,fa,nlevels=15)
-image(x,y,fa)
-persp(x,y,fa)
--- scribe/README.md
+++
@@ -1,5 +0,0 @@
-# STA 380 (Part 2): Scribe Notes
-
-
-Scribe notes will be posted here as they arrive.
-
