f0f24ffa49f8d6fa20772364b7df1f652f2ef364
wenduowang <wenduo.wang@hotmail.com>
2016-09-15 08:23:51 -0500

    Add homework 2

---
+++ python/MSBA/textanalytics/HW2/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+__author__ = "Billy Yuan, Nikita Lakhotia, Stuti Maddan, Tyler Nicolas, Wenduo Wang"
+__copyright__ = "Well, knowledge is open to curious minds."
+__license__ = "GPL-3.0"
+__version__ = "0.1"
+__maintainer__ = "Wenduo Wang"
+__email__ = "wenduo.wang@utexas.edu"
+__status__ = "development"
+
+import numpy as np
+from time import time
+
