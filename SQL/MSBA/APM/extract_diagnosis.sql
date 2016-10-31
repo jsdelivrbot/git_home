9dc62967ced18313422afdcff2e83a4286433c13
wenduowang <wenduo.wang@hotmail.com>
2016-10-30 21:07:36 -0500

    Fixed syntax

--- SQL/MSBA/APM/extract_diagnosis.sql
+++ SQL/MSBA/APM/extract_diagnosis.sql
@@ -1,50 +1,83 @@
-SELECT admissions.subject_id AS subject_id,
-       admissions.hadm_id AS hadm_id,
-       admissions.admittime,
-       admissions.dischtime,
-       admissions.deathtime,
-       admissions.admission_type,
-       admissions.diagnosis,
-       STRING_AGG(diagnoses_icd.seq_num, ' ') AS diagnosis_seq,
-       STRING_AGG(diagnoses_icd.icd9_code, ' ') AS diagnoses,
-       STRING_AGG(procedures_icd.seq_num, ' ') AS procedures_seq,
-       STRING_AGG(procedures_icd.icd9_code, ' ') AS procedures,
-       patients.gender AS gender,
-       patients.dob AS DOB,
-       patients.dod AS DOD,
-       patients.dod_hosp AS DOD_HOSP,
-       patients.dod_ssn AS DOD_SSN,
-       patients.expire_flag,
-       STRING_AGG(prescription.drug_type, ' ') AS drug_types,
-       STRING_AGG(prescription.drug, ' ') AS drugs
+COPY (
+	WITH diagnosis AS (
+		SELECT subject_id,
+			   hadm_id,
+			   STRING_AGG(diagnoses_icd.seq_num::character, ' ') AS diagnosis_seq,
+			   STRING_AGG(diagnoses_icd.icd9_code, ' ') AS diagnoses
+		FROM diagnoses_icd
+		GROUP BY subject_id,
+				 hadm_id
+	),
+	procedures AS (
+		SELECT subject_id,
+			   hadm_id,
+			   STRING_AGG(procedures_icd.seq_num::character, ' ') AS procedures_seq,
+			   STRING_AGG(procedures_icd.icd9_code, ' ') AS procedures
+		FROM procedures_icd
+		GROUP BY subject_id,
+				 hadm_id
+	),
+	prescription AS (
+		SELECT subject_id,
+			   hadm_id,
+			   STRING_AGG(prescriptions.drug_type, ' ') AS drug_types,
+			   STRING_AGG(prescriptions.drug, ' ') AS drugs
+		FROM prescriptions
+		GROUP BY subject_id,
+				 hadm_id
+	),
+	admission AS (
+		SELECT subject_id,
+			   hadm_id,
+			   admittime,
+			   dischtime,
+			   deathtime,
+			   admission_type,
+			   diagnosis
+		FROM admissions
+	),
+	patient AS (
+		SELECT subject_id,
+			   gender,
+			   dob,
+			   dod,
+			   dod_hosp,
+			   dod_ssn,
+			   expire_flag
+		FROM patients
+	)
+	SELECT admission.subject_id AS subject_id,
+		   admission.hadm_id AS hadm_id,
+		   gender,
+		   dob,
+		   dod,
+		   dod_hosp,
+		   dod_ssn,
+		   expire_flag,
+		   admittime,
+		   dischtime,
+		   deathtime,
+		   admission_type,
+		   admission.diagnosis AS initial_diagnosis,
+		   diagnosis_seq,
+		   diagnoses,
+		   procedures_seq,
+		   procedures.procedures AS procedures,
+		   drug_types,
+		   drugs
+	FROM admission,
+		 patient,
+		 diagnosis,
+		 procedures,
+		 prescription
+	WHERE admission.subject_id = patient.subject_id
+		  AND admission.hadm_id = diagnosis.hadm_id
+		  AND admission.subject_id = diagnosis.subject_id
+		  AND admission.subject_id = procedures.subject_id
+		  AND admission.hadm_id = procedures.hadm_id
+		  AND admission.subject_id = prescription.subject_id
+		  AND admission.hadm_id = prescription.hadm_id
 
-FROM admissions,
-     diagnoses_icd,
-     procedures_icd,
-     patients,
-     prescription
-
-WHERE admissions.subject_id = diagnoses_icd.subject_id
-      AND admissions.hadm_id = diagnoses_icd.hadm_id
-      AND admissions.subject_id = procedures_icd.subject_id
-      AND admissions.hadm_id = procedures_icd.hadm_id
-      AND admissions.subject_id = patients.subject_id
-      AND admissions.subject_id = prescription.subject_id
-      AND admissions.hadm_id = prescription.hadm_id
-
-GROUP BY admissions.subject_id,
-         admissions.hadm_id,
-         admissions.admittime,
-         admissions.dischtime,
-         admissions.deathtime,
-         admissions.admission_type,
-         admissions.diagnosis
-         patients.gender,
-         patients.dob,
-         patients.dod,
-         patients.dod_hosp,
-         patients.dod_ssn,
-         patients.expire_flag
-
-ORDER BY subject_id ASC,
-         hadm_id ASC
+	ORDER BY subject_id ASC,
+			 hadm_id ASC
+) TO 'E:\Data\mimic3\postgresql\extract_diagnosis.csv' WITH CSV DELIMITER ',' HEADER;
